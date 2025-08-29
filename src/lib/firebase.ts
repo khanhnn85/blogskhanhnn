b3bc0a7a0b840e717f750dfb72b581c49211e676
Nguyen Ngoc Khanh <khanhnn85@gmail.com>
2025-08-29 06:59:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  "projectId": "khanhnn-insights",
-  "appId": "1:163641152572:web:e5d343947e69ac6c03df39",
-  "storageBucket": "khanhnn-insights.firebasestorage.app",
-  "apiKey": "AIzaSyA_nMxp05bE_E_pIkmlY4JT9TCO5piJ7Ho",
-  "authDomain": "khanhnn-insights.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "163641152572"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
